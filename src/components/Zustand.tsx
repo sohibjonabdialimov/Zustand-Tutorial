3310ce65e3845c8628613aa3b9f79f4ea3e9b510
sohibjonabdialimov <sohibjonabdialimov6@gmail.com>
2024-03-16 12:43:13 +0500

    change

--- src/components/Zustand.tsx
+++ src/components/Zustand.tsx
@@ -5,10 +5,12 @@ const Zustand = () => {
   const increment = useCounter((state) => state.increment);
   const decrement = useCounter((state) => state.decrement);
   const reset = useCounter((state) => state.reset);
+  const double = useCounter((state) => state.double);
   return (
     <>
       <h1>{count}</h1>
       <button onClick={increment}>increment</button>
+      <button onClick={() => double(2)}>double</button>
       <button onClick={decrement}>decrement</button>
       <button onClick={reset}>reset</button>
     </>
--- src/store.ts
+++ src/store.ts
@@ -4,7 +4,8 @@ type CountType = {
   count: number,
   increment: () => void,
   decrement: () => void,
-  reset: () => void
+  reset: () => void,
+  double: (num: number) => void
 }
 
 export const useCounter = create<CountType>((set) => (
@@ -12,6 +13,7 @@ export const useCounter = create<CountType>((set) => (
     count: 0,
     increment: () => set((state) => ({count: state.count + 1})),
     decrement: () => set((state) => ({count: state.count - 1})),
-    reset: () => set(() => ({count: 0}))
+    reset: () => set(() => ({count: 0})),
+    double: (num) => set((state) => ({count: state.count + num}))
   }
 ))
